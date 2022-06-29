8c11c80e975a3c61d6b49052b039283d03f9dff2
Egar King David <egarkingdavid007@gmail.com>
2022-06-29 13:46:39 -0700

    adv

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# Author - Egar King David
-
 def remove_char_at(str, n):
-    v = ""
-    for j in range(len(str)):
-        if j != n:
-            v = v + str[j]
-            return (v)
+    str_copy = str
+    if (str):
+        str = str[:n] + str[n + 1:]
+    if (n < 0):
+        str = str_copy
+    return (str)
